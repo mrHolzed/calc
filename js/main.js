46d47230336ef9d5bd503dabdf3763cf47639b20
mrHolzed <solopov@MacBook-Pro-Konstantin.local>
2018-12-08 23:34:47 +0300

    0.15

--- js/main.js
+++ js/main.js
@@ -3,6 +3,21 @@ $(document).ready(function(){
     $('.calculator__wrapper').toggle();
   });
 
+  $("#mail").submit(function() { //Change
+		var th = $(this);
+		$.ajax({
+			type: "POST",
+			url: "/mail.php", //Change
+			data: th.serialize()
+		}).done(function() {
+			alert("Thank you!");
+			setTimeout(function() {
+				// Done Functions
+				th.trigger("reset");
+			}, 1000);
+		});
+		return false;
+	});
 
   function myFunction() {
       var x = document.getElementById("tt").value;
