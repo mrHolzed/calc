1a410fc3c0aa5e2a4e0d5f50b24079f10e6073c6
mrHolzed <solopov@MacBook-Pro-Konstantin.local>
2018-12-08 23:43:51 +0300

    0.16

--- js/main.js
+++ js/main.js
@@ -3,21 +3,28 @@ $(document).ready(function(){
     $('.calculator__wrapper').toggle();
   });
 
-  $("#mail").submit(function() { //Change
-		var th = $(this);
-		$.ajax({
-			type: "POST",
-			url: "/mail.php", //Change
-			data: th.serialize()
-		}).done(function() {
-			alert("Thank you!");
-			setTimeout(function() {
-				// Done Functions
-				th.trigger("reset");
-			}, 1000);
-		});
-		return false;
-	});
+  function _(id){ return document.getElementById(id); }
+    function submitForm(){
+    	_("mybtn").disabled = true;
+    	_("status").innerHTML = 'please wait ...';
+    	var formdata = new FormData();
+    	formdata.append( "n", _("n").value );
+    	formdata.append( "e", _("e").value );
+    	formdata.append( "m", _("m").value );
+    	var ajax = new XMLHttpRequest();
+    	ajax.open( "POST", "example_parser.php" );
+    	ajax.onreadystatechange = function() {
+    		if(ajax.readyState == 4 && ajax.status == 200) {
+    			if(ajax.responseText == "success"){
+    				_("my_form").innerHTML = '<h2>Thanks '+_("n").value+', your message has been sent.</h2>';
+    			} else {
+    				_("status").innerHTML = ajax.responseText;
+    				_("mybtn").disabled = false;
+    			}
+    		}
+    	}
+    	ajax.send( formdata );
+    }
 
   function myFunction() {
       var x = document.getElementById("tt").value;
